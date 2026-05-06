7b35c3b925dc16e6001c27b77b1f04d2245b063c
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-06 12:13:38 +0530

    Create otpRoutes.js

---
+++ server/routes/otpRoutes.js
@@ -0,0 +1,13 @@
+// ============================================
+// OTP ROUTES
+// ============================================
+
+const express = require('express');
+const router = express.Router();
+const otpController = require('../controllers/otpController');
+
+router.post('/send', otpController.sendOTP);
+router.post('/verify', otpController.verifyOTP);
+router.post('/resend', otpController.resendOTP);
+
+module.exports = router;
